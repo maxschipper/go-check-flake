8cedeb868357ae4c704c45c98e43cfe110a47e22
Max S <150921823+maxschipper@users.noreply.github.com>
2026-02-16 15:49:19 +0100

    build: add package to flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,40 @@
 {
   description = "";
   inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
+
   outputs =
     { nixpkgs, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        function: nixpkgs.lib.genAttrs supportedSystems (system: function nixpkgs.legacyPackages.${system});
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          go
-          gopls
-          gotools
-          golangci-lint
-          golangci-lint-langserver
-          gofumpt
-        ];
-      };
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            gotools
+            golangci-lint
+            golangci-lint-langserver
+          ];
+        };
+      });
+
+      packages = forAllSystems (pkgs: {
+        default = pkgs.buildGoModule {
+          pname = "go-check-flake";
+          version = "0.0.1";
+          src = ./.;
+          vendorHash = null;
+        };
+      });
     };
 }
